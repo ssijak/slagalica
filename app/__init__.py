c0156046c75195a2980518e42db4c88b8767e1e4
Perun <avaricak@gmail.com>
2013-12-07 11:48:42 +0100

    added config.py and flask_oauthlib

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ __author__ = 'Perun'
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_oauthlib.client import OAuth
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+oauth = OAuth(app)
 
 from app import views, model
---
+++ config.py
@@ -0,0 +1,12 @@
+__author__ = 'Perun'
+
+
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+#sqlalchemy db uri
+SQLALCHEMY_DATABASE_URI = "add path here"
+
+FACEBOOK_APP_ID = 'app_id'
+FACEBOOK_APP_SECRET = 'app_secret'
